b003a8734b2005d7a0b23bca4bbe21eb122b5a5e
Yastrebov Maxim <yamax343@gmail.com>
2018-12-14 22:32:21 +0700

    Step #0: Create consumer-application and producer-application

---
+++ producer/src/main/java/ru/myastrebov/demo/spring/cloud/contract/ProducerApplication.java
@@ -0,0 +1,13 @@
+package ru.myastrebov.demo.spring.cloud.contract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+
+}
